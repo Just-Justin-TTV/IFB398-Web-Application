7479bf45da28eb015e5f7f0e93764670e74b4736
justinthegamer <justinsingh2004@gmail.com>
2025-10-14 04:20:58 +1000

    feat: Add impact_rating field to interventions model and update URL patterns

--- app1/urls.py
+++ app1/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Corrected to home page path
     path('calculator/', views.calculator, name='calculator'),
-    path('calculator/results/', views.calculator_results, name='calculator_results'),
+    #path('calculator/results/', views.calculator_results, name='calculator_results'),
     path('django_browser_reload/', include('django_browser_reload.urls')),
 
     # Authentication
