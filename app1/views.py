91a640a4ec6a114653fd48030ffb0a73b7d29c62
justinthegamer <justinsingh2004@gmail.com>
2025-10-21 19:10:56 +1000

    Revert "Initial commit"
    This reverts commit b080e0aadfda0c439ae870f56ab59a2ce4e145ca.

--- app1/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def helloworld(request):
-    return render(request, "helloworld.html")
--- readme.md
+++
@@ -1,5 +0,0 @@
-# How to run
-
-1. `pip install -r requirements.txt`
-2. `python manage.py runserver`
-3. `source testiews/bin/activate` # only for me
